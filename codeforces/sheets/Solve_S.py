4a419585679c6c593a104dab9004e9241250561a
ahmedsaleh.datalab@gmail.com <ahmedsaleh.datalab@gmail.com>
2026-04-16 18:17:35 +0200

    Solve S : Interval

---
+++ codeforces/sheets/Solve_S.py
@@ -0,0 +1,14 @@
+# S : Interval
+
+number = float(input())
+
+if 0 <= number <= 25:
+    print("Interval [0,25]")
+elif 25 < number <= 50:
+    print("Interval (25,50]")
+elif 50 < number <= 75:
+    print("Interval (50,75]")
+elif 75 < number <= 100:
+    print("Interval (75,100]")
+else:
+    print("Out of Intervals")
